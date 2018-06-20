79015f3a57fd0c4d72094515df5ccc8cb26a0fe6
3panda <3panda@3panda.local>
2018-06-20 14:01:33 +0900

    Prototype completed

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# DirectoryViewer
-# DirectoryViewer
+ディレクトリをツリー状に表示する
+
+## 前提条件
+- Python 3.6.4以上
+
+## 利用方法
+
+```
+directory_viewer.py <path>
+```
+- path
+表示したいディレクトリのPath
---
+++ directory_viewer.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import sys
+import glob
+
+
+def main(path, layer=0, is_root=False):
+    tab = u" "
+    if is_root is True:
+        file_path = glob.glob(path)
+        root_name = ','.join(file_path)
+        print(root_name)
+
+    files = glob.glob(path + '/*')
+
+    for file in files:
+        # get dir or file path
+        file_paths = file.split('/')
+        # get dir or file name
+        print(tab * (layer + 1) + "|-" + file_paths.pop())
+        if os.path.isdir(file):
+            # case dir (recall)
+            main(file, layer + 1)
+
+
+if __name__ == '__main__':
+
+    if len(sys.argv) == 2:
+        main(path=sys.argv[1], is_root=True)
+    else:
+        print("usage.")
+        print("{filename} <path(ex:/root)>>"
+              .format(filename=sys.argv[0]))
